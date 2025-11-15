84af33800f5a9bf279d74f06a1a7c61b3192f773
fady <fady@DESKTOP-MDQ512F>
2025-11-15 18:20:25 +0200

    Finish_register_and_login

---
+++ Domain/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,3 @@
+﻿namespace Domain.Exceptions
+{    public sealed class UnauthorizedException(string msg = "Invalid email or password") : Exception(msg) { }
+}
---
+++ Shared/Models/ResetPasswordRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+    public class ResetPasswordRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Required(ErrorMessage = "Password confirmation is required")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
